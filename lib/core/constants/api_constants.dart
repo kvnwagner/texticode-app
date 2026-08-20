1b6aec1026ad5a2aa3ce6afca61e0a11693898f8
stevanwagner1-sys <stevanwagner1@email.com>
2026-08-19 22:12:07 -0500

    Arreglos de la vista Gestion Usuarios

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -6,7 +6,7 @@ class ApiConstants {
   //   1. Averigua la IP local de tu PC (ipconfig en Windows, ifconfig en Mac/Linux).
   //   2. Cambia esta constante por, por ejemplo: 'http://192.168.1.15:3001/api'
   //   3. Asegúrate de que el celular y el PC estén en la MISMA red WiFi.
-  static const String baseUrl = 'http://192.168.0.7:3001/api';
+  static const String baseUrl = 'http://192.168.0.5:3001/api';
 
   static const String usuarios = '$baseUrl/usuarios';
   static const String auth = '$baseUrl/auth';
